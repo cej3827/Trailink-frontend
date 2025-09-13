7389fc4868ea6407ee1ef8af6e4e3724f001c184
dmswls <56065660+cej3827@users.noreply.github.com>
2025-09-13 21:48:23 +0900

    update

--- src/app/category/[categoryId]/page.tsx
+++ src/app/category/[categoryId]/page.tsx
@@ -34,6 +34,7 @@ export default async function PublicCategoryPage({ params }: PageProps) {
         // <PublicCategoryClient 
         // initialBookmarks={bookmarks}
         // />
+        <></>
     )
 }
 
